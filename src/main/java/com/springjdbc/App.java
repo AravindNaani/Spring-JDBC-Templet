a4aa9d68ab67c6fb65eea47ed10d7cb4d7783ff0
AravindNaani <aravindbujji7@gmail.com>
2024-01-08 17:57:35 +0530

    Implemented Retrive functionality in Spring-JDBC-Templet 1.Implemented for select entrair records in table 2.implemented for select single record in table

--- src/main/java/com/springjdbc/App.java
+++ src/main/java/com/springjdbc/App.java
@@ -1,8 +1,12 @@
 package com.springjdbc;
 
+
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class App {
 
@@ -16,7 +20,23 @@ public class App {
 //		int update = jdbcTemplet.update("update student set name=? where id=?","Aravi",5);
 //		System.out.println(update);
 		
-		jdbcTemplet.update("delete from student where id=?",5);
+//		jdbcTemplet.update("delete from student where id=?",5);
+		
+
+		RowMapper<Student> rowMapper = (rs,r)->{
+			Student student = new Student();
+			student.setId(rs.getInt(1));
+			student.setName(rs.getString(2));
+			student.setMarks(rs.getInt(3));
+			return student;
+		};
+		
+//		List<Student> studentList = jdbcTemplet.query("select * from student", rowMapper);
+//		studentList.stream().forEach((st)->System.out.println(st));
+		
+		Student student = jdbcTemplet.queryForObject("select * from student where id=1", rowMapper);
+		System.out.println(student);
+		
 	}
 
 }
